bd8ad721561209882667bb75133a2c1a5575327f
ThienMenh <dinhthienmenh1505@gmail.com>
2024-07-31 20:00:12 +0700

    init

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ContactManagement.Models;
+
+namespace ContactManagement.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ContactManagement.Models.Phones> Phones { get; set; } = default!;
+    }
+}
---
+++ Models/Phones.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ContactManagement.Models
+{
+    public class Phones
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string firstName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string lastName { get; set; }
+        [StringLength(250)]
+        public string? email { get; set; }
+        [Required]
+        [StringLength(10)]
+        public string phoneNumber { get; set; }
+        [Required]
+        public int UserId { get; set; }
+    }
+}
